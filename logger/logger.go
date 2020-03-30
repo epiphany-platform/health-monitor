50bd220b14c04f23eea22df65d4fef5e5d727b20
smeadows-abb <62151069+smeadows-abb@users.noreply.github.com>
2020-03-30 07:56:08 -0400

    Changed imports to match Epiphany platform.

--- logger/logger.go
+++ logger/logger.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"log"
 	"log/syslog"
+	"path/filepath"
 	"runtime"
 )
 
@@ -28,41 +29,41 @@ func Close() error {
 // Crit logs a message with severity LOG_CRIT
 func Crit(m string) error {
 	_, fn, line, _ := runtime.Caller(1)
-	return sysLog.Crit(fmt.Errorf("%s:%d %v", fn, line, m).Error())
+	return sysLog.Crit(fmt.Errorf("%s:%d %v", filepath.Base(fn), line, m).Error())
 }
 
 // Alert logs a message with severity LOG_ALERT
 func Alert(m string) error {
 	_, fn, line, _ := runtime.Caller(1)
-	return sysLog.Alert(fmt.Errorf("%s:%d %v", fn, line, m).Error())
+	return sysLog.Alert(fmt.Errorf("%s:%d %v", filepath.Base(fn), line, m).Error())
 }
 
 // Debug logs a message with severity LOG_DEBUG
 func Debug(m string) error {
 	_, fn, line, _ := runtime.Caller(1)
-	return sysLog.Debug(fmt.Errorf("%s:%d %v", fn, line, m).Error())
+	return sysLog.Debug(fmt.Errorf("%s:%d %v", filepath.Base(fn), line, m).Error())
 }
 
 // Emerg logs a message with severity LOG_EMERG
 func Emerg(m string) error {
 	_, fn, line, _ := runtime.Caller(1)
-	return sysLog.Emerg(fmt.Errorf("%s:%d %v", fn, line, m).Error())
+	return sysLog.Emerg(fmt.Errorf("%s:%d %v", filepath.Base(fn), line, m).Error())
 }
 
 // Err logs a message with severity LOG_ERR
 func Err(m string) error {
 	_, fn, line, _ := runtime.Caller(1)
-	return sysLog.Err(fmt.Errorf("%s:%d %v", fn, line, m).Error())
+	return sysLog.Err(fmt.Errorf("%s:%d %v", filepath.Base(fn), line, m).Error())
 }
 
 // Info logs a message with severity LOG_INFO
 func Info(m string) error {
 	_, fn, line, _ := runtime.Caller(1)
-	return sysLog.Info(fmt.Errorf("%s:%d %v", fn, line, m).Error())
+	return sysLog.Info(fmt.Errorf("%s:%d %v", filepath.Base(fn), line, m).Error())
 }
 
 // Warning logs a message with severity LOG_WARNING
 func Warning(m string) error {
 	_, fn, line, _ := runtime.Caller(1)
-	return sysLog.Warning(fmt.Errorf("%s:%d %v", fn, line, m).Error())
+	return sysLog.Warning(fmt.Errorf("%s:%d %v", filepath.Base(fn), line, m).Error())
 }
